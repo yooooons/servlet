e5152e362f3774aed026b8eec800b7df0d3f15e0
YS <yooooonnf@gmail.com>
2022-02-01 23:32:15 +0900

    서블릿, JSP, MVC 패턴 commit

---
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,52 @@
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "memberSaveServlet",urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        //자바코드->html
+        //지금까지 서블릿과 자바 코드만으로 HTML을 만들어보았다. 서블릿 덕분에 동적으로 원하는 HTML을 마음껏 만들 수 있다.
+        /*그런데, 코드에서 보듯이 이것은 매우 복잡하고 비효율 적이다. 자바 코드로 HTML을 만들어 내는 것 보다
+        차라리 HTML 문서에 동적으로 변경해야 하는 부분만 자바 코드를 넣을 수 있다면 더 편리할 것이다.
+        이것이 바로 템플릿 엔진이 나온 이유이다. 템플릿 엔진을 사용하면 HTML 문서에서 필요한 곳만 코드를
+        적용해서 동적으로 변경할 수 있다.
+        템플릿 엔진에는 JSP, Thymeleaf, Freemarker, Velocity등이 있다.*/
+        PrintWriter w = response.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                " <li>id=" + member.getId() + "</li>\n" +
+                " <li>username=" + member.getUsername() + "</li>\n" +
+                " <li>age=" + member.getAge() + "</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
+                "</body>\n" +
+                "</html>");
+
+    }
+}
